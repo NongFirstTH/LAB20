ecc4fbb3a57fa57ee1fa7757a8da3addd0a28e16
Your Name <you@example.com>
2023-02-21 16:42:16 +0700

    OKOK

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
+double overlap(Rect *R1,Rect *R2){
+double R,L,U,D;
+R = min(((*R1).x)+(*R1).w,((*R2).x)+(*R2).w);
+L = max((*R1).x,(*R2).x);
+U = min((*R1).y,(*R2).y);
+D = max(((*R1).y)-(*R1).h,((*R2).y)-(*R2).h);
+double A = (R-L)*(U-D);
+if(A>0){
+return A;
+}else{
+return 0;
+}
 
 
 }
